2a611250a02e9cc0e26714097209f5f5fbe1982d
kicky1 <krzysiekwicki1@gmail.com>
2022-11-11 23:34:21 +0100

    Revert all changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,6 @@
-import '@/css/tailwind.css'
-import '@/css/prism.css'
-import 'katex/dist/katex.css'
-
-import '@fontsource/inter/variable-full.css'
-
-import { ThemeProvider } from 'next-themes'
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
-
-import siteMetadata from '@/data/siteMetadata'
-import Analytics from '@/components/analytics'
-import LayoutWrapper from '@/components/LayoutWrapper'
-import { ClientReload } from '@/components/ClientReload'
-
-const isDevelopment = process.env.NODE_ENV === 'development'
-const isSocket = process.env.SOCKET
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
-      <Head>
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-      </Head>
-      {isDevelopment && isSocket && <ClientReload />}
-      <Analytics />
-      <LayoutWrapper>
-        <Component {...pageProps} />
-      </LayoutWrapper>
-    </ThemeProvider>
-  )
+  return <Component {...pageProps} />
 }
