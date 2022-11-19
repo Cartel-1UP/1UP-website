73d011655f017944757f5a49ebb44478339066ee
kicky1 <krzysiekwicki1@gmail.com>
2022-11-20 00:08:10 +0100

    Add: Cards grid

---
+++ components/CardsGrid/CardsGrid.tsx
@@ -0,0 +1,37 @@
+'use client'
+import { AspectRatio, Card, Container, Image, SimpleGrid, Text } from '@mantine/core';
+import useStyles from '.';
+import { mockdata } from './data';
+
+
+
+
+
+export function ArticlesCardsGrid() {
+  const { classes } = useStyles();
+
+  const cards = mockdata.map((article) => (
+    <Card key={article.title} p="md" radius="md" component="a" href="#" className={classes.card}>
+      <Text color="dimmed" size="xs" transform="uppercase" weight={700} mt="md">
+        Tags: [ {article.category} ]
+      </Text>
+      <AspectRatio ratio={1920 / 1080}>
+        <Image src={article.image} />
+      </AspectRatio>
+      <Text color="dimmed" size="xs" transform="uppercase" weight={700} mt="md">
+      {article.author} - {article.date}
+      </Text>
+      <Text className={classes.title} mt={5}>
+        {article.title}
+      </Text>
+    </Card>
+  ));
+
+  return (
+    <Container py="xl">
+      <SimpleGrid cols={2} breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
+        {cards}
+      </SimpleGrid>
+    </Container>
+  );
+}
