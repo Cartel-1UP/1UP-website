c3ab1df21d8d26203d91024ea80109be26516c0d
kicky1 <krzysiekwicki1@gmail.com>
2022-11-19 20:28:23 +0100

    Add: Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Image from 'next/image'
-import styles from './page.module.css'
 
 export default function Home() {
   return (
     <div>
-      <main>Hello OneUp!</main>
+      <main></main>
       <footer></footer>
     </div>
   )
---
+++ components/Header/data.tsx
@@ -0,0 +1,34 @@
+import { IconArrowGuide, IconBrandZoom, IconBrush, IconMicrophone, IconNews, IconStar } from '@tabler/icons';
+
+export const mockdata = [
+    {
+      icon: IconNews,
+      title: 'News',
+      description: 'You can find all the latest news from around the world here.',
+    },
+    {
+      icon: IconBrandZoom,
+      title: 'Videos',
+      description: 'Video about gaming, review of video games, reviews of video games, tips for video games, information about video games.',
+    },
+    {
+      icon: IconStar,
+      title: 'Reviews',
+      description: 'Find out what are the most popular games rewies or how to make your own.',
+    },
+    {
+      icon: IconArrowGuide,
+      title: 'Guides',
+      description: 'A good guide for you who play game for a long time.',
+    },
+    {
+      icon: IconBrush,
+      title: 'Art',
+      description: 'Users artwork about games.',
+    },
+    {
+      icon: IconMicrophone,
+      title: 'Intervies',
+      description: 'Interviews with people from hive engine world',
+    },
+  ];
