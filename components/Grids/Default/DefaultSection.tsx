9521bb3225824993ae706a2dde871e1de6627953
kicky1 <krzysiekwicki1@gmail.com>
2023-01-07 22:53:15 +0100

    #Add: pagination

--- components/Grids/Default/DefaultSection.tsx
+++ components/Grids/Default/DefaultSection.tsx
@@ -1,45 +1,67 @@
 'use client'
 import { AspectRatio, Card, Center, Container, Grid, Image, SimpleGrid, Space, Text, Title } from '@mantine/core'
-import { Suspense, useEffect, useState } from 'react'
+import { Suspense } from 'react'
 import useStyles from '.'
-import api from '../../../utils/api'
+import { usePostsStore } from '../../../zustand/stores/usePostsStore'
 import BlogPagination from '../../Pagination/Pagination'
 
 export function ArticlesCardsGrid() {
   const { classes, theme } = useStyles()
-  const [articles, setArticles] = useState<any>([])
+  const posts = usePostsStore((state: { posts: any; }) => state.posts)
+  const lastUser = usePostsStore((state: { lastUser: any; }) => state.lastUser)
+  console.log(lastUser)
+  // type Posts = {
+  //   tag: string,
+  //   nextBlog?: {
+  //     start_author: string,
+  //     perma_link: string
+  //   }
+  // }
   
-  async function getPosts(tag: string) {
-    try {
-      const { data } = await api.post('trending', tag )      
-      setArticles(data.result)
+  // async function getPosts(props: Posts) {
+  //   try {
+  //     const { data } = await api.post('trending', {tag: props.tag} ) 
+  //     const user = {
+  //       author: data.result[3].author,
+  //       link: data.result[3].perma_link
+  //     }     
+  //     setPosts(data.result)
+  //     setLastUser(user)
 
-    } catch (e:any) {
-      console.log(e)
-    }
-  }
+  //   } catch (e:any) {
+  //     console.log(e)
+  //   }
+  // }
 
-  useEffect(() => {
-    getPosts("hive-102223")
-  }, [])
+  // useEffect(() => {
+  //   getPosts({tag: "hive-102223"})
+  // }, [])
 
-  const cards = articles.map((article: any) => (
+  const cards = posts.map((article: any) => {
+    const date = new Date(article.created);
+    const formattedDate = date.toLocaleDateString('en-US', {
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric'
+    });
+
+    return (
       <Card key={article.post_id} p="md" radius="md" component="a" href="#" className={classes.card}>
         <Grid grow>        
           <Grid.Col span={3}>
           <AspectRatio ratio={4/3}>
-            <Image src={article.json_metadata.image[0]} />
+            <Image src={article.json_metadata.image ? article.json_metadata.image[0] : null} />
           </AspectRatio>
           </Grid.Col>
           <Grid.Col span={9}>
             <Container>
               <Text color="dimmed" size="xs" transform="uppercase" weight={700}>
-                {article.author} - {article.created}
+                {article?.author} - {formattedDate}
               </Text>
             </Container>
             <Container>
               <Text className={classes.title} mt={5}>
-                {article.title}
+                {article?.title}
               </Text>
               <Text color="dimmed" size="sm" weight={600}  mt={10}>
                 {/* {article.body} */}
@@ -47,18 +69,19 @@ export function ArticlesCardsGrid() {
             </Container>
             <Container>
               <Text color="dimmed" size="xs" transform="uppercase" weight={600} mt={20}>
-                Tags: [ {article.category} ]
+                Tags: [ {article?.category} ]
               </Text>
             </Container>
             <Container>
               <Text color="dimmed"  className={classes.price} >
-                ${article.payout} | comments: {article.children}
+                ${article?.payout} | comments: {article?.children}
               </Text>
             </Container>
           </Grid.Col>
         </Grid>
       </Card>
-  ))
+    )}
+    )
 
   return (
 
