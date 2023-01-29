8915d6267b835e7d5fabb38a3b6db781fd4542f8
kicky1 <krzysiekwicki1@gmail.com>
2023-01-29 21:35:26 +0100

    Add: fetching data from supabase + loader

--- components/Grids/Main/MainSection.tsx
+++ components/Grids/Main/MainSection.tsx
@@ -1,10 +1,10 @@
 'use client'
 import { Carousel } from '@mantine/carousel';
-import { Box, Container, Text, Title, useMantineTheme } from '@mantine/core';
+import { Box, Center, Container, Loader, Text, Title, useMantineTheme } from '@mantine/core';
 import { useMediaQuery } from '@mantine/hooks';
+import { useEffect, useState } from 'react';
 import useStyles from '.';
 import { supabase } from '../../../lib/supabaseClient';
-import { mockdata } from './data';
 
 interface CardProps {
   image: string;
@@ -39,20 +39,58 @@ function Card({ image, title, category }: CardProps) {
 }
 
 export function MainCardsGrid() {
+  const [loading, setLoading] = useState(true);
+  const [maincards, setMaincards] = useState(['']);
+
   const { classes } = useStyles();
   const theme = useMantineTheme();
   const mobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm}px)`);
-  const slides = mockdata.map((item, index) => (
-    <Carousel.Slide key={index}>
-      <Card {...item} />
-    </Carousel.Slide>
-  ));
 
-  getServerSideProps()
+
+  
+
+  useEffect(() => {
+    fetchMaincards()
+  }, [])
+
+  const fetchMaincards = async () => {
+    try {
+
+      setLoading(true);
+
+      const {data, error} = await supabase
+      .from("maincards")
+      .select("*")
+
+      if (error) throw error
+
+      const slides:any = data.map((item, index) => (
+        <Carousel.Slide key={index}>
+          <Card {...item} />
+        </Carousel.Slide>
+      ));
+
+      setMaincards(slides)
+      
+    } catch (error: any) {
+      alert(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
 
   return (
     <Container fluid className={classes.gradientBot}>
       <Container size="xl" pt={0}>
+      {loading ? 
+      <Center>
+          <Box sx={{height: 400}}>
+            <Center>
+                <Loader size="xl" color="dark"/> 
+            </Center>
+          </Box>
+          </Center>
+        :
         <Carousel
           slideSize="33.33%"
           breakpoints={[{ maxWidth: 'xs', slideSize: '100%', slideGap: 3 }]}
@@ -61,20 +99,11 @@ export function MainCardsGrid() {
           slidesToScroll={mobile ? 1 : 3}
           loop
         >
-          {slides}
+
+          {maincards}
         </Carousel>
+ }
       </Container>
     </Container>
   );
 }
-
-
-
-export async function getServerSideProps() {
-  let { data } = await supabase.from('maincards').select()
-
-  console.log(data)
-
-  return data
-}
-
