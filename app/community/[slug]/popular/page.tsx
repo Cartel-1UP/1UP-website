3b302076981cc804b38b6f1a272aa79be8ef8d80
kicky1 <krzysiekwicki1@gmail.com>
2023-02-08 22:42:53 +0100

    Change url

--- app/community/[slug]/popular/page.tsx
+++ app/community/[slug]/popular/page.tsx
@@ -2,7 +2,6 @@
 
 import { Suspense } from "react";
 import { CommunityGrid } from "../../../../components/Grids/Community/Community";
-import { MainPage } from "../../../../components/Grids/CommunityPage/CommunityPage";
 import { DirectSection } from "../../../../components/Grids/Direct/DirectSection";
 
 
@@ -16,7 +15,6 @@ export default function Page({params}: {params: { slug: string}}) {
   return (
     <div>
         <Suspense>
-        <MainPage  image={params.slug}/>
         <CommunityGrid/>
         <DirectSection tag={params.slug} type={'trending'} name={'Popular'}/>
         </Suspense>
