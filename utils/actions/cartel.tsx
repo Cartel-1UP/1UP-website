a72fea4379d10cae96f0967f6d49320754b66761
kicky1 <krzysiekwicki1@gmail.com>
2023-02-11 09:35:41 +0100

    Fix: carousel structure

---
+++ utils/actions/cartel.tsx
@@ -0,0 +1,13 @@
+import { supabase } from '../../lib/supabaseClient';
+
+export const fetchMaincards = async () => {
+    try {
+      const { data } = await supabase
+        .from("maincards")
+        .select("*");
+      
+      return data;
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
