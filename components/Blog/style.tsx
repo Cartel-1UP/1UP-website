972dd21deae14d36445a521b26cf13fa3932f93c
kicky1 <krzysiekwicki1@gmail.com>
2023-02-25 14:49:45 +0100

    Add: fetching post data, page for one blog

---
+++ components/Blog/style.tsx
@@ -0,0 +1,33 @@
+import { createStyles } from '@mantine/styles'
+
+const useStyles = createStyles((theme) => ({
+
+    footer: {
+        background: ' #072f37'
+      },
+    
+      inner: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: `${theme.spacing.md}px ${theme.spacing.md}px`,
+    
+        [theme.fn.smallerThan('sm')]: {
+          flexDirection: 'column',
+        },
+      },
+    
+      links: {
+        [theme.fn.smallerThan('sm')]: {
+          marginTop: theme.spacing.lg,
+          marginBottom: theme.spacing.sm,
+        },
+      },
+
+      gradientBot: {
+        background: 'linear-gradient(to bottom, white, #072f37);'
+      }
+
+}))
+
+export default useStyles
