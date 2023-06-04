d7437d08212f666fdf7d9f2669701f34b7345b2e
Krzysztof Wicki <s171703@student.pg.edu.pl>
2023-06-04 13:35:31 +0200

    Add comment textArea

--- components/Blog/BlogPage.tsx
+++ components/Blog/BlogPage.tsx
@@ -4,22 +4,23 @@ import { useQuery } from 'react-query';
 import { getComments, getPost } from '../../utils/actions/posts';
 import { getUserProfile } from '../../utils/actions/user';
 import { CommentCard } from './CommentCard/CommentCard';
+import Editor from './CommentTextInput/CommentTextInput';
 import { ContentCard } from './ContentCard/ContentCard';
 import useStyles from './style';
 
-interface Props{
+interface Props {
   id: string,
   username: string
 }
 
-export function BlogPage({...props}: Props) {
+export function BlogPage({ ...props }: Props) {
   const { classes, theme } = useStyles();
-  
+
   const { data: data } = useQuery('postData', () => getPost({
     permlink: props.id,
     author: props.username,
   }));
-  
+
   const { data: comments } = useQuery('commentsData', () => getComments({
     permlink: props.id,
     author: props.username,
@@ -28,43 +29,44 @@ export function BlogPage({...props}: Props) {
   const { data: user } = useQuery('userData', () => getUserProfile({
     author: props.username,
   }));
-  
-  if(data && user && comments){
+
+  if (data && user && comments) {
     return (
       <Container fluid className={classes.default}>
         <Container size="xl">
           <Grid>
             <ContentCard article={data} user={user} />
-            <CommentCard comments={comments} permlink={props.id}/>
+            <Editor />
+            <CommentCard comments={comments} permlink={props.id} />
           </Grid>
-          </Container>
+        </Container>
       </Container>
     );
   }
 
-  return(
+  return (
     <Container fluid className={classes.default}>
-      <Container size="lg" sx={{height: '80vh'}}>
+      <Container size="lg" sx={{ height: '80vh' }}>
         <SimpleGrid cols={1} pt={25} spacing={0} breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
           <Card withBorder p="md" radius={0} className={classes.cardHeader}>
             <Grid grow>
               <Grid.Col span={10}>
-                <Skeleton height={24}/>
+                <Skeleton height={24} />
               </Grid.Col>
               <Grid.Col span={2}>
-                <Skeleton height={24}/>
+                <Skeleton height={24} />
               </Grid.Col>
             </Grid>
           </Card>
-          <Card  withBorder p="md" radius={0} className={classes.card}>
-            <Skeleton height='40vh'/>
+          <Card withBorder p="md" radius={0} className={classes.card}>
+            <Skeleton height='40vh' />
           </Card>
-          <Card  withBorder p="md" mb={25} radius={0} className={classes.cardFooter}>
-            <Skeleton height={24}/>
+          <Card withBorder p="md" mb={25} radius={0} className={classes.cardFooter}>
+            <Skeleton height={24} />
           </Card>
-        </SimpleGrid> 
+        </SimpleGrid>
       </Container>
-  </Container>
+    </Container>
   )
- 
+
 }
---
+++ components/Blog/CommentTextInput/CommentTextInput.tsx
@@ -0,0 +1,136 @@
+import { ActionIcon, Button, Card, Grid, Group, SimpleGrid, Space, Textarea } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
+import { IconBold, IconHeading, IconItalic, IconLink, IconPhotoDown } from '@tabler/icons';
+import React, { useRef, useState } from 'react';
+import useStyles from './style';
+
+type Tag = {
+    name: string;
+    type: 'link' | 'heading' | 'bold' | 'italic' | 'image';
+};
+
+const Editor = () => {
+    const { classes, theme } = useStyles();
+    const laptop = useMediaQuery(`(max-width: ${theme.breakpoints.md}px)`);
+
+    const [markdown, setMarkdown] = useState('');
+    const [previewModalOpen, setPreviewModalOpen] = useState(false);
+    const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+
+    const handleMarkdownChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setMarkdown(event.target.value);
+    };
+
+    const handleTagButtonClick = (tagName: string, type: Tag['type']) => {
+        if (textareaRef.current) {
+            const textarea = textareaRef.current;
+            const start = textarea.selectionStart || 0;
+            const end = textarea.selectionEnd || 0;
+            const value = textarea.value;
+
+            let modifiedValue = value.slice(0, start);
+            const selectedText = value.slice(start, end);
+
+            switch (type) {
+                case 'link':
+                    modifiedValue += `[${selectedText}]()`;
+                    break;
+                case 'heading':
+                    modifiedValue += `# ${selectedText}`;
+                    break;
+                case 'bold':
+                    modifiedValue += `**${selectedText}**`;
+                    break;
+                case 'italic':
+                    modifiedValue += `*${selectedText}*`;
+                    break;
+                case 'image':
+                    modifiedValue += `![${selectedText}]()`;
+                    break;
+                default:
+                    modifiedValue += selectedText;
+                    break;
+            }
+
+            modifiedValue += value.slice(end);
+
+            setMarkdown(modifiedValue);
+            textarea.focus();
+            textarea.setSelectionRange(start, start + selectedText.length);
+        }
+    };
+
+    const handleReply = () => {
+        setPreviewModalOpen(true);
+    };
+
+
+
+    const markdownComponents = {
+        a: ({ href, children }: any) => (
+            <a href={href} target="_blank" rel="noopener noreferrer">
+                {children}
+            </a>
+        ),
+        h1: ({ node, children }: any) => <h1>{children}</h1>,
+        h2: ({ node, children }: any) => <h2>{children}</h2>,
+        strong: ({ node, children }: any) => <strong>{children}</strong>,
+        em: ({ node, children }: any) => <em>{children}</em>,
+        img: ({ src, alt }: any) => <img src={src} alt={alt} />,
+        p: ({ node, children }: any) => <p>{children}</p>,
+        ul: ({ node, children }: any) => <ul>{children}</ul>,
+        li: ({ node, children }: any) => <li>{children}</li>,
+    };
+
+    return (
+        <>
+            <Grid grow sx={{ width: '-webkit-fill-available' }}>
+                <Grid.Col span={laptop ? 12 : 9}>
+                    <SimpleGrid cols={1} mt={0} spacing={0} breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
+                        <Card withBorder p="md" radius={0}>
+                            <Grid grow>
+                                <Grid.Col>
+                                    <Group spacing={3}>
+                                        <ActionIcon variant="default" onClick={() => handleTagButtonClick('Link', 'link')}>
+                                            <IconLink size="1rem" />
+                                        </ActionIcon>
+                                        <ActionIcon variant="default" onClick={() => handleTagButtonClick('Heading', 'heading')}>
+                                            <IconHeading size="1rem" />
+                                        </ActionIcon>
+                                        <ActionIcon variant="default" onClick={() => handleTagButtonClick('Bold', 'bold')}>
+                                            <IconBold size="1rem" />
+                                        </ActionIcon>
+                                        <ActionIcon variant="default" onClick={() => handleTagButtonClick('Italic', 'italic')}>
+                                            <IconItalic size="1rem" />
+                                        </ActionIcon>
+                                        <ActionIcon variant="default" onClick={() => handleTagButtonClick('Image', 'image')}>
+                                            <IconPhotoDown size="1rem" />
+                                        </ActionIcon>
+                                    </Group>
+                                </Grid.Col>
+                                <Grid.Col>
+                                    <Space w="sm" />
+                                    <Textarea value={markdown} onChange={handleMarkdownChange} ref={textareaRef} autosize minRows={4} />
+                                    <Space w="sm" />
+                                </Grid.Col>
+                                {/* <Grid.Col>
+                                    <Button onClick={handlePreviewModalOpen}>Show Preview</Button>
+                                </Grid.Col> */}
+                                <Grid.Col>
+                                    <Button onClick={handleReply}>Reply</Button>
+                                </Grid.Col>
+                            </Grid>
+                        </Card>
+                    </SimpleGrid>
+                </Grid.Col>
+                <Grid.Col span={laptop ? 12 : 3}></Grid.Col>
+            </Grid>
+
+            {/* <Modal size={'xl'} opened={previewModalOpen} onClose={handlePreviewModalClose} title="Comment preview">
+                <ReactMarkdown components={markdownComponents}>{markdown}</ReactMarkdown>
+            </Modal> */}
+        </>
+    );
+};
+
+export default Editor;
