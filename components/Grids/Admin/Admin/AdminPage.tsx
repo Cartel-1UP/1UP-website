6d0b252e0bfbd09ce609725ddefc82ca7989afea
Krzysztof Wicki <s171703@student.pg.edu.pl>
2023-06-25 19:25:51 +0200

    Add admin panel

--- components/Grids/Admin/Admin/AdminPage.tsx
+++ components/Grids/Admin/Admin/AdminPage.tsx
@@ -1,14 +1,34 @@
 'use client'
-import { SimpleGrid, Space } from '@mantine/core'
-import useStyles from '../style'
+import { Card, SimpleGrid, Space, Tabs, Title } from '@mantine/core';
+import { useState } from 'react';
+import { AdminMainCards } from '../AdminMainCards/AdminMainCards';
+import useStyles from './style';
 
 export function AdminPage() {
   const { classes, theme } = useStyles()
+  const [activeTab, setActiveTab] = useState<string | null>('first');
+
   return (
     <>
       <Space h="xl" />
       <SimpleGrid cols={1} mt={0} spacing={0} breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
-        Admin
+        <Card withBorder p="md" radius={0} className={classes.cardHeader}>
+          <Title order={2}>
+            Dashboard
+          </Title>
+        </Card>
+        <Card withBorder p="md" radius={0} className={classes.cardBody}>
+          <Tabs value={activeTab} onTabChange={setActiveTab}>
+            <Tabs.List>
+              <Tabs.Tab value="first">Main cards</Tabs.Tab>
+              <Tabs.Tab value="second">Users choice</Tabs.Tab>
+            </Tabs.List>
+            <Tabs.Panel value="first">
+              <AdminMainCards />
+            </Tabs.Panel>
+            <Tabs.Panel value="second">Second panel</Tabs.Panel>
+          </Tabs>
+        </Card>
       </SimpleGrid>
       <Space h="xl" />
     </>
---
+++ components/Grids/Admin/Admin/style.tsx
@@ -0,0 +1,29 @@
+import { createStyles } from '@mantine/styles'
+
+const useStyles = createStyles((theme, _params, getRef) => ({
+
+  card: {
+    borderColor: '#e2e8f0d2',
+    borderWidth: 1,
+  },
+
+  default: {
+    background: `linear-gradient(to bottom, #072f37 0%, #E9ECEF 10%, #E9ECEF 10%, #E9ECEF 100%)`,
+  },
+
+  cardHeader: {
+    borderColor: '#e2e8f0d2',
+    borderWidth: 1,
+    borderRadius: '10px 10px 0 0'
+  },
+
+  cardBody: {
+    borderColor: '#e2e8f0d2',
+    borderWidth: 1,
+  },
+
+
+
+}))
+
+export default useStyles
