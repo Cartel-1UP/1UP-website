78d54bb9507d21aa46a64d3f51e6859492bb0afd
Krzysztof Wicki <s171703@student.pg.edu.pl>
2023-07-18 14:57:06 +0200

    Fix ednge runtime

--- app/community/[slug]/post/[username]/[id]/page.tsx
+++ app/community/[slug]/post/[username]/[id]/page.tsx
@@ -4,7 +4,6 @@ import { BlogPage } from "../../../../../../components/Blog/BlogPage";
 import { DefaultSnackbar } from "../../../../../../components/Grids/DefaultSnackbar/DefaultSnackbar";
 import { useNotifiactionStore } from "../../../../../../zustand/stores/useNotificationStore";
 
-export const runtime = 'edge';
 
 export default function Page({ params }: { params: { id: string, username: string } }) {
   const snackbars = useNotifiactionStore((state) => state.snackbars);
