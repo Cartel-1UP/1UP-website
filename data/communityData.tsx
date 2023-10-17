8d39d0d5e1508514a7c50edde910746e9f2b72e1
kicky1 <krzysztofwicki1@gmail.com>
2023-10-17 22:47:17 +0200

    feat: add partners for mobile version

--- data/communityData.tsx
+++ data/communityData.tsx
@@ -18,54 +18,67 @@ export const comumnityData = [
   //   tag: 'hive-102223',
   // },
   {
+    name: 'Splinterlands',
     image: splinterlands.src,
     tag: 'hive-13323',
   },
   {
+    name: 'DCrops',
     image: dcrops.src,
     tag: 'hive-115712',
   },
   {
+    name: 'DCity',
     image: dcity.src,
     tag: 'hive-198197',
   },
   {
+    name: 'MusicForLife',
     image: mu.src,
     tag: 'hive-196251',
   },
   {
+    name: 'RisingStar',
     image: risingstar.src,
     tag: 'hive-195370',
   },
   {
+    name: 'Woo',
     image: woo.src,
     tag: 'hive-191258',
   },
   {
+    name: 'AstralRevelations',
     image: astralrevelations.src,
     tag: 'none',
   },
   {
+    name: 'CraftInk',
     image: craftInk.src,
     tag: 'none',
   },
   {
+    name: 'CryptoCompany',
     image: cryptocompany.src,
     tag: 'hive-196769',
   },
   {
+    name: 'GLS',
     image: gls.src,
     tag: 'none',
   },
   {
+    name: 'HK',
     image: hk.src,
     tag: 'none',
   },
   {
+    name: 'KOD',
     image: kod.src,
     tag: 'none',
   },
   {
+    name: 'PsyberX',
     image: psyber.src,
     tag: 'hive-168109',
   },
