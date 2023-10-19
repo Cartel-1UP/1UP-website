a63d7954bdf6bd9de609a5c16e9991e53e40c070
kicky1 <krzysiekwicki1@gmail.com>
2023-10-19 10:01:38 +0200

    test: post page

--- app/community/[slug]/post/[username]/[id]/page.tsx
+++ app/community/[slug]/post/[username]/[id]/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 
+import { BlogPage } from '@/components/Blog/BlogPage';
 
 export default function Page({ params }: { params: { id: string; username: string } }) {
   const permlink = params.id
   const author = params.username
   return (
     <>
-      {/* <BlogPage permlink={permlink} author={author} /> */}
+      <BlogPage permlink={permlink} author={author} />
     </>
   )
 }
