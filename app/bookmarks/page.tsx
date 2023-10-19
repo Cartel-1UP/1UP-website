d0f8884809a4f5978542dcbe648760576ce8c4f2
kicky1 <krzysiekwicki1@gmail.com>
2023-10-19 11:34:35 +0200

    update next

--- app/bookmarks/page.tsx
+++ app/bookmarks/page.tsx
@@ -7,12 +7,8 @@ import {
 } from '@mantine/core'
 import { useMediaQuery } from "@mantine/hooks"
 
-
-
 export default function Page() {
   const isMd = useMediaQuery(`(max-width: 1000px)`)
-
-
   return (
     <>
       <Container fluid pb={20} bg={'linear-gradient(to bottom, #072f37 0%, #E9ECEF 33%, #E9ECEF 33%, #E9ECEF 100%)'}>
