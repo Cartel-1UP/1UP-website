58f4e78c0a6048a68e247d02d5475a9f5363d08f
kicky1 <krzysztofwicki1@gmail.com>
2023-10-22 18:36:11 +0200

    test: twitter

--- data/twitterData.tsx
+++ data/twitterData.tsx
@@ -1,7 +1,7 @@
 export const tagToTwitterAccount: Record<string, string> = {
     'hive-13323': 'splinterlands',
     'hive-115712': 'd_crops',
-    'hive-191258': 'WrestOrgOnline',
+    'hive-191258': 'wrestorgonline',
     'hive-196769': 'CryptoCompany9',
     'hive-168109': 'PSYBERXGAME'
     // Add more mappings as needed
