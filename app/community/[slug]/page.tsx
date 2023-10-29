ab6fdf46bbcd89db88935fbe24f1ab2469d8e501
kicky1 <krzysiekwicki1@gmail.com>
2023-10-29 15:17:31 +0100

    feat: update subpage with community

--- app/community/[slug]/page.tsx
+++ app/community/[slug]/page.tsx
@@ -2,19 +2,37 @@
 
 import { CommunityPage } from '@/components/CommunityPage/CommunityPage'
 import { RecommendedSection } from '@/components/Sections/Recommended/RecommendedSection'
-import { Container } from '@mantine/core'
+import { Container, Space } from '@mantine/core'
+import { useMediaQuery } from '@mantine/hooks'
 
 export const runtime = 'experimental-edge'
 
 export default function Page({ params }: { params: { slug: string } }) {
+  const isMd = useMediaQuery(`(max-width: 1000px)`)
   return (
     <>
-      <CommunityPage tag={params.slug} image={params.slug} />
-      <Container fluid bg={'linear-gradient(to bottom, #f3f3f3, #072f37)'}>
-        <Container size="xl">
-          <RecommendedSection />
-        </Container>
-      </Container>
+      {
+        isMd ? (
+          <>
+            <CommunityPage tag={params.slug} image={params.slug} />
+            <RecommendedSection />
+          </>
+        ) : (
+          <>
+            <Container fluid bg={`linear-gradient(to bottom, #072f37 0%, #072f37 10%, #f3f3f3 30%, #f3f3f3 100%)`}>
+              <Container size="xl">
+                <Space h="xl" />
+                <CommunityPage tag={params.slug} image={params.slug} />
+              </Container>
+            </Container>
+            <Container fluid bg={'linear-gradient(to bottom, #f3f3f3, #072f37)'}>
+              <Container size="xl">
+                <Space h="xl" />
+                <RecommendedSection />
+              </Container>
+            </Container>
+          </>
+        )}
     </>
   )
 }
