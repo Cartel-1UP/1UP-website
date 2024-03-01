040b09153bf8a4ec891528ce6aeb906b21e57c78
Wicki Krzysztof <krzysztof_wicki@sevenet.pl>
2024-03-01 14:24:19 +0100

    feat: update styles

--- components/About/Communities.tsx
+++ components/About/Communities.tsx
@@ -1,11 +1,13 @@
 'use client'
 import { comumnityData } from '@/data/communityData'
+import useSettings from '@/utils/methods/useSettings'
 import { Card, Grid, Image, Space, Text } from '@mantine/core'
 import Link from 'next/link'
 import useStyles from './style'
 
 export function Communities() {
   const { classes } = useStyles()
+  const { ...settings } = useSettings()
 
   return (
     <>
@@ -16,31 +18,42 @@ export function Communities() {
           fontFamily: 'Segoe UI, sans-serif',
         }}
         ta="left"
-        fz={48}
+        fz={settings.isMd ? 28 : 48}
         fw={700}
-        mt={100}
+        mt={settings.isMd ? 30 : 100}
       >
         We are working with
       </Text>
       <Space h={20} />
       <Grid>
-        {
-          comumnityData.map((item, index) => (
-            <Grid.Col span={2} key={index}>
-              {item.tag != 'none' ?
-                <Link href={item?.tag}>
-                  <Card p="md" bg={'radial-gradient(#2ecde6 30%, #162947 130%)'} radius="md" className={classes.card}>
-                    <Image src={item.image} alt={item.name} fit='scale-down' />
-                  </Card>
-                </Link> :
-                <Card p="md" bg={'radial-gradient(#2ecde6 30%, #162947 130%)'} radius="md" className={classes.card}>
-                  <Image src={item.image} alt={item.name} fit='scale-down' />
+        {comumnityData.map((item, index) => (
+          <Grid.Col span={settings.isMd ? 3 : 2} key={index}>
+            {item.tag !== 'none' ? (
+              <Link href={item?.tag}>
+                <Card
+                  p="md"
+                  bg={'radial-gradient(#2ecde6 30%, #162947 130%)'}
+                  radius={settings.isMd ? "md" : "lg"}
+                  className={classes.card}
+
+                >
+                  <Image src={item.image} alt={item.name} fit="scale-down" />
                 </Card>
-              }
-            </Grid.Col>
-          ))
-        }
-      </Grid >
+              </Link>
+            ) : (
+              <Card
+                p="md"
+                bg={'radial-gradient(#2ecde6 30%, #162947 130%)'}
+                radius="md"
+                className={classes.card}
+
+              >
+                <Image src={item.image} alt={item.name} fit="scale-down" />
+              </Card>
+            )}
+          </Grid.Col>
+        ))}
+      </Grid>
     </>
   )
 }
