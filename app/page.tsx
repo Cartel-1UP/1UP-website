a07509f8f1f41e4502b323a679f1ece796e5be3e
Wicki Krzysztof <krzysiekwicki1@gmail.com>
2024-03-01 14:24:19 +0100

    feat: update styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 'use client'
 
 import { AboutPage } from '@/components/About/AboutPage'
+import useSettings from '@/utils/methods/useSettings'
 import { Container } from '@mantine/core'
 
 export default function Home() {
+  const { ...settings } = useSettings()
   return (
     <>
       <Container fluid bg={'radial-gradient(#125c6b 0%, #072f37 70%)'} pb={25}>
-        <Container size="xl" pt={'90px'}>
+        <Container size="xl" pt={settings.isMd ? '10px' : '90px'}>
           <AboutPage />
         </Container>
       </Container>
