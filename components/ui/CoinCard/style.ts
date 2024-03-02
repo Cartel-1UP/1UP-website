50a3bec2b29ddf992206fe4764a812982a306b4e
Wicki Krzysztof <krzysztof_wicki@sevenet.pl>
2024-03-02 22:02:48 +0100

    feat: add cards with token pricing

---
+++ components/ui/CoinCard/style.ts
@@ -0,0 +1,16 @@
+import { createStyles } from '@mantine/styles'
+
+const useStyles = createStyles((theme) => ({
+  card: {
+    borderRadius: '8px',
+    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
+    border: '1px solid rgba(255, 255, 255, 0.1)',
+    transition: 'box-shadow 0.3s, background 0.4s',
+    '&:hover': {
+      boxShadow: '0 4px 20px rgba(0, 0, 0, 0.5)', // Adjust shadow properties as needed
+      background: 'linear-gradient(#2ecde6 -100%, #162947 60%)', // Adjust the gradient color slightly
+    },
+  },
+}))
+
+export default useStyles
